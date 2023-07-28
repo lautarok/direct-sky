62ed8fa6d1e7216a74995f30ddcd1d7a1d5aadd2
unknown <lautarokza@gmail.com>
2023-07-28 10:07:23 -0300

    Commit

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -202,7 +202,7 @@
         <div
           ref='messageField'
           class='message_field'
-          contentEditable='plaintext-only'
+          :contentEditable='{"plaintext-only": !loading && !loadingMessages}'
           @keypress.enter.prevent='submitMessage'
           @keypress.enter.shift=''
         ></div>
